3c8684d82fabc4e58c9bc946ca4f34a09975d9d2
akshaydongare <dongare_akshay@ymail.com>
2019-03-29 12:31:17 +0530

    new

---
+++ app/Http/Controllers/API/PassportController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Socialite;
+
+class PassportController extends Controller
+{
+
+    public $successStatus = 200;
+
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(){
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
+            flash('You are logged in!','success');
+            return redirect('home');
+        }
+        else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['name'] =  $user->name;
+
+        return response()->json(['success'=>$success], $this->successStatus);
+    }
+
+    /**
+     * details api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getDetails()
+    {
+        $user = Auth::user();
+        return response()->json(['success' => $user], $this->successStatus);
+    }
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('github')->user();
+
+        return redirect('home')->with($user);
+        $token = $user->token;
+		$refreshToken = $user->refreshToken; // not always provided
+		$expiresIn = $user->expiresIn;
+
+		// OAuth One Providers
+		$token = $user->token;
+		$tokenSecret = $user->tokenSecret;
+
+		// All Providers
+		$user->getId();
+		$user->getNickname();
+		$user->getName();
+		$user->getEmail();
+		$user->getAvatar();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::post('login', 'API\PassportController@login');
+Route::post('register', 'API\PassportController@register');
+Route::get('login/github', 'API\PassportController@redirectToProvider')->name('github_login');
+Route::get('login/github/callback', 'API\PassportController@handleProviderCallback');
+Route::get('logout', function(){
+	Auth::logout();
+})->name('logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('getDetails', 'HomeController@getDetails')->name('details');
